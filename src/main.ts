fcfe6ce391a71d7fa49881f7b3eccbb89c3b3c0a
mic-v <michaelvictorino@gmail.com>
2023-09-09 01:17:03 -0400

    Fixed up index.html, initial landing screen for knight moves

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,81 @@ import { setupCounter } from './counter.ts'
 import { ChessBoard } from './knight-moves.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  <main>
+    <h1>Knight Moves Display</h1>
+    <section id="main-content">
+      <div id="chess">
+          <div id="chessboard">
+          </div>
+          <ul id="board-column">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+          </ul>
+          <ul id="board-row">
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+            <li>E</li>
+            <li>F</li>
+            <li>G</li>
+            <li>H</li>
+          </ul>
+      </div>
+      <div id="io-box">
+        <div class="flex-row">
+          <div class="flex-row">
+          <label for="start-square">Start Square: </label>
+          <input type="text" name="start-square id="start-square" pattern="[a-zA-Z]{1}[1-8]{1}" required></input>
+          </div>
+          <div class="flex-row">
+            <label for="end-square">End Square: </label>
+            <input type="text" name="end-square id="end-square" pattern="[a-zA-Z]{1}[1-8]{1}" required></input>
+          </div>
+          <button id="input-button">Enter</button>
+        </div>
+        <ul id="output">
+          <li>blah</li>
+        </ul>
+        <div id="controls" class="flex-row">
+          <button><</button>
+          <button>Play</button>
+          <button>></button>
+        </div>
+      </div>
+    </section>
+    
+  </main>
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+`;
+
+function generateHtmlChessBoard() {
+  const htmlBoard = document.getElementById('chessboard');
+  
+  for(let i = 0; i < 8; i++) {
+    for(let j = 0; j < 8; j++) {
+      const square = document.createElement('div');
+      const id = i + '' + j;
+      square.setAttribute('id', id);
+
+      if((i + j) % 2 === 0) {
+        square.setAttribute('class', 'square-color-1');
+      } else {
+        square.setAttribute('class', 'square-color-2');
+      }
+
+      htmlBoard?.appendChild(square);
+    }
+  }
+}
+
+generateHtmlChessBoard();
 
 
 const board = new ChessBoard();
